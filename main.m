e2a2b484075a8aea4f84633beea6a468226d2e8b
Liu Zicheng <90845197+58120201@users.noreply.github.com>
2022-09-06 09:38:30 +0800

    main demo

---
+++ main.m
@@ -0,0 +1,97 @@
+addpath(genpath(cd))
+clear
+clc
+%% parameter seting
+opts.mu = 1e-6;
+opts.rho = 1.1;
+opts.max_iter = 500;
+opts.DEBUG = 1;
+
+%% main
+% img_name = 'Indian_pines';
+% T = importdata(['./data-HSI/' img_name '_noised.mat']);
+% T_real = importdata(['./data-HSI/' img_name '_corrected.mat']);
+% gt = importdata(['./data-HSI/' img_name '_gt.mat']);
+% CTrain = [6 144 84 24 50 75 3 49 2 97 247 62 22 130 38 10];
+% lambda = 5e-3;
+
+img_name = 'Salinas';
+T = importdata(['.\data-HSI\' img_name '_corrected.mat']);
+T_real = importdata(['.\data-HSI\' img_name '_corrected.mat']);
+gt = importdata(['.\data-HSI\' img_name '_gt.mat']);
+CTrain = [200 373 198 140 268 396 358 1127 620 328 107 193 92 107 727 180];
+lambda = 5e-3;
+
+% img_name = 'SalinasA';
+% T = importdata(['./data-HSI/' img_name '_noised.mat']);
+% T_real = importdata(['./data-HSI/' img_name '_corrected.mat']);
+% gt = importdata(['./data-HSI/' img_name '_gt.mat']);
+% CTrain = [39 0 0 0 0 0 0 0 0 134 62 153 67 80 ];
+% lambda = 1e-2;
+
+% img_name = 'BotswanaA';
+% % T = importdata(['.\data-HSI\' img_name '_noised.mat']);
+% T = importdata(['.\data-HSI\' img_name '_noised.mat']);
+% T_real = importdata(['.\data-HSI\' img_name '_corrected.mat']);
+% gt = importdata(['.\data-HSI\' img_name '_gt.mat']);
+% CTrain = [9 10 0 0 0 0 26 0 31 25 31 18 27 5];
+% lambda = 5e-1;
+
+% img_name = 'PaviaUA';
+% T = importdata(['.\data-HSI\' img_name '_noised.mat']);
+% T_real = importdata(['.\data-HSI\' img_name '.mat']);
+% gt = importdata(['.\data-HSI\' img_name '_gt.mat']);
+% CTrain = [228 267 210 108 70 503 133 180 86];
+
+
+CTrain = round(CTrain/2);
+%lambda = 1/sqrt(n3*max(n1,n2));
+%lambda = 1/sqrt(max(n3,n1*n2));
+
+[n1,n2,n3] = size(T);
+
+%different algorithms
+% opts.fun = 'oddfunc' ; opts.gamma = 0.01;
+opts.fun = 'etp' ;  opts.gamma = 10;
+% opts.fun = 'laplace' ; opts.gamma = 0.1;
+% opts.fun = 'exponential' ; opts.gamma = 0.05 ;
+% opts.fun = 'lp' ;        opts.gamma = 0.5 ;
+% opts.fun = 'scad' ;      opts.gamma = 100 ;
+% opts.fun = 'cappedl1' ; opts.gamma = 1000 ;
+% opts.fun = 'mcp' ; opts.gamma = 10 ;
+% opts.fun = 'geman' ;  opts.gamma = 10 ;
+% opts.fun = 'logarithm' ; opts.gamma = 10 ;
+
+% LAMBDA = [5e-3 1e-2 1e-1 1];
+% OA=[];
+% Kappa=[];
+% for i = 1:length(LAMBDA)
+%         tic
+%          [Lhat,Shat] = trpca_explr_plus(T_real,LAMBDA(i),opts);
+%         time = toc;
+%         [OA_SVM1, OA_SVM2, AA_SVM1, AA_SVM2, Kappa_SVM1, Kappa_SVM2,ave_TPR_SVM1,ave_TPR_SVM2] = Classification_V2(T_real,Lhat,gt,3,CTrain);
+%         OA = [OA, OA_SVM2];
+%         Kappa = [Kappa,Kappa_SVM2];
+% end
+% OA
+
+tic
+% [TRec,  S] = trpca_explr(T, lambda,opts);
+[TRec,  S] = trpca_explr_plus(T_real, lambda,opts);
+time = toc;
+%% classification
+%[OA_SVM1, OA_SVM2, AA_SVM1, AA_SVM2, Kappa_SVM1, Kappa_SVM2,ave_TPR_SVM1,ave_TPR_SVM2] = Classification_V2(T_real,T,gt,10,CTrain);
+[OA_SVM1, OA_SVM3, AA_SVM1, AA_SVM3, Kappa_SVM1, Kappa_SVM3,ave_TPR_SVM1,ave_TPR_SVM3] = Classification_V2(T_real,TRec,gt,10,CTrain);
+
+% %% save result
+% file = fopen('classification_result.txt','w');
+% fprintf(file,'  %s  %s  %s  %s  %s  %s  %s \n','time',  'OA_SVM_org',  'OA_SVM_rev',  'AA_SVM_org',  'AA_SVM_rev', 'Kappa_SVM_org', 'Kappa_SVM_rev');
+% fprintf(file,'  %f  %f  %f  %f  %f  %f  %f \n',[time  OA_SVM1  OA_SVM2  AA_SVM1  AA_SVM2  Kappa_SVM1  Kappa_SVM2]);
+% fclose(file);
+% %% 展示用，可注释
+% if n3>3
+%     subplot(121);
+%     imagesc(T(:,:,100));
+%     subplot(122);
+%     imagesc(TRec(:,:,100));
+% end;
